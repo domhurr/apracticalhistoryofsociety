e3623329166999f4e7d059db150af3ff318a1daf
Dominic Hurr <dom@Dominics-MacBook-Air.local>
2020-05-13 21:38:12 +0100

    Update site title and description.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Practical history of society',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'An abridged version of Piketty\'s "Capital and Ideology", to provide a deeper understanding of why today\'s society is structured the way it is.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
